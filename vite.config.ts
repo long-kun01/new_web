eb79cb78394e65881273eb881b5614f00a5f2272
long-kun01 <cleverfly1314@gmail.com>
2025-11-01 19:32:59 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/dejiuweb/',   // 你的仓库名，必须以 / 开头、/ 结尾
   plugins: [react()],
+  build: {
+    outDir: 'docs'      // 让打包产物输出到 docs 目录
+  }
 })
